69caef5b3f4e159081d946bf4fcafb141b6ae19a
ANNMAINAWANGARI <wangariann371@gmail.com>
2022-09-19 01:21:56 +0300

    done with twilio-sms-api

--- bgServices/src/server.ts
+++ bgServices/src/server.ts
@@ -3,11 +3,13 @@ import express from 'express'
 //import SendProjectEmails from './EmailService/ProjectServices'
 import SendEmails from './EmailService/AccountCreation'
 import SenderEmails from './EmailService/ParcelUpdateService'
+import SendSMS from './TwilioSMSService/twilio'
 const app= express()
 
 const run =async()=>{
   await SendEmails()
   await SenderEmails()
+  await SendSMS()
   //await SendProjectEmails()
 }
 run()
